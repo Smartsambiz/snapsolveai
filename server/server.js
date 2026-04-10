2b544c2eaab2aaccca019b5e62f170c27d7a9fdd
Samuel Dauda <smartsam9981@gmail.com>
2026-04-10 01:19:58 +0100

    still fixing cors issues

--- server/server.js
+++ server/server.js
@@ -17,14 +17,20 @@ const corsOptions = {
   credentials: true,
 };
 
-
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://snapsolveai-xi.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
-// app.options('*', cors(corsOptions));
 app.use(express.json());
 
 app.use("/api", solveRoutes);
+
+
 
 app.use("/", (req, res)=>{
     res.send("Snapsolve AI Backend Running");
