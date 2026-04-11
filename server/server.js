bacb8316442aba4ebbb5aeef711319d817da566a
Samuel Dauda <smartsam9981@gmail.com>
2026-04-11 20:15:22 +0100

    still cors issues

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://snapsolveai-xi.vercel.app', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://snapsolveai-xi.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
